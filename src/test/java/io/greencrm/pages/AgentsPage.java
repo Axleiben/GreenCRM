4fe9a7e219c654d682adf340a9cb3689540abe96
BRITENET <michal.banaszek@gmail.com>
2023-10-17 17:01:47 +0200

    dodanie obsługi otwarcia okna zmiany statusu

--- src/test/java/io/greencrm/pages/AgentsPage.java
+++ src/test/java/io/greencrm/pages/AgentsPage.java
@@ -1,9 +1,15 @@
 package io.greencrm.pages;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 
 public class AgentsPage {
 
@@ -37,8 +43,20 @@ public class AgentsPage {
     @FindBy(xpath = "//tbody/tr/td/div[text()='Edytuj']")
     private WebElement editAgentButton;
 
+    @FindBy(xpath = "//tbody/tr/td/div/span[@class='editItemText_e7aMq']")
+    private List<WebElement> statusLabel;
 
-     public AgentsPage (WebDriver driver) {PageFactory.initElements(driver,this);}
+    @FindBy (xpath = "//tbody/tr/td/div/span[@aria-label='edit']")
+    private List  <WebElement> statusEditButton;
+
+    private WebDriver driver;
+
+
+
+     public AgentsPage (WebDriver driver) {
+         PageFactory.initElements(driver,this);
+         this.driver = driver;
+     }
 
     public void openNewAgentForm(){
 
@@ -76,5 +94,15 @@ public class AgentsPage {
 
     public void openEditForm (){
          editAgentButton.click();
+    }
+
+    public void openStatusWindow(){
+        Actions actions = new Actions(driver);
+        statusLabel.stream().findFirst().
+                ifPresent(e -> actions.moveToElement(e).perform());
+
+        statusEditButton.stream().findFirst()
+                .ifPresent(WebElement::click);
+
     }
 }
--- src/test/java/io/greencrm/tests/Testing.java
+++ src/test/java/io/greencrm/tests/Testing.java
@@ -1,11 +1,18 @@
 package io.greencrm.tests;
 
 import io.greencrm.pages.*;
+import org.checkerframework.checker.units.qual.A;
+import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.interactions.Actions;
 
@@ -20,7 +27,7 @@ public class Testing {
         options.setPageLoadStrategy(PageLoadStrategy.NONE);
 
 
-        WebDriver driver = new ChromeDriver();
+         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://test.greencrm.dev");
         driver.manage().window().maximize();
@@ -28,30 +35,26 @@ public class Testing {
 
         LoginPage loginPage = new LoginPage(driver);
         SidebarPage sidebarPage = new SidebarPage(driver);
-
+        AgentsPage agentsPage = new AgentsPage(driver);
+        Actions actions =  new Actions(driver);
         loginPage.logIn();
-
-        //driver.findElement(By.cssSelector("a[href='/agents']")).click();
-
        sidebarPage.openAgentsPage();
 
-    //driver.findElements(By.xpath("//tbody/tr/td/div[text()='Edytuj']")).stream().findFirst().ifPresent(WebElement::click);
-        AgentsPage agentsPage = new AgentsPage(driver);
-
-        agentsPage.openEditForm();
+       agentsPage.searchingStatus("Aktywne");
 
 
 
+//    Optional<WebElement> element=  driver.findElements(By.xpath("//tbody/tr/td/div/span[@class='editItemText_e7aMq']"))
+//            .stream()
+//            .findFirst();
+//            element.ifPresent(e -> actions.moveToElement(e).perform());
+//
+//            //driver.findElements(By.xpath("//tbody/tr/td/div[text()='Edytuj']")).stream().findFirst().ifPresent(WebElement::click);
+//
+//
+//   driver.findElements(By.xpath("//tbody/tr/td/div/span[@aria-label='edit']")).stream().findFirst().ifPresent(WebElement::click);
 
-
-
-
-
-
-
-
-
-    }
+}
 
 
 
